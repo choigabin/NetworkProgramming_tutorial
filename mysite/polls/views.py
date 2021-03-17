d4af4993a3c31af0657d619e0fe9c16a446e8bb8
choigabin <63983505+choigabin@users.noreply.github.com>
2021-03-17 23:21:52 +0900

    polls/views index render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    # HTTP로 응답
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
